0b7454bfcc9b15cb755ba223e09b535577098bde
cain.chu <cainmaila>
2023-08-31 15:14:26 +0800

    view2

--- src/App2.tsx
+++ src/App2.tsx
@@ -1,4 +1,4 @@
-window.__CHILD_APP_ROOT__ = "https://cainmaila.github.io/ex-micro-app-child";
+window.__CHILD_APP_ROOT__ = 'https://cainmaila.github.io/ex-micro-app-child'
 
 export default function App2() {
   return (
@@ -6,8 +6,8 @@ export default function App2() {
       <div>
         <h2>App2</h2>
         <child-app></child-app>
-        <child-app></child-app>
+        <child-app-view2></child-app-view2>
       </div>
     </>
-  );
+  )
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,11 @@
 /// <reference types="vite/client" />
 declare interface Window {
-  __CHILD_APP_ROOT__?: string;
+  __CHILD_APP_ROOT__?: string
 }
 
 declare namespace JSX {
   interface IntrinsicElements {
-    "child-app": unknown;
+    'child-app': unknown
+    'child-app-view2': unknown
   }
 }
